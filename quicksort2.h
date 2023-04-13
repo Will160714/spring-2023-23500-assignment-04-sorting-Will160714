634b34cc5b5ca0315ceb279b4089c4c477531a6b
Will160714 <95445527+Will160714@users.noreply.github.com>
2023-04-13 11:11:12 -0400

    Complete

---
+++ quicksort2.h
@@ -0,0 +1,9 @@
+#pragma once
+#include <iostream>
+#include <vector>
+#include <string>
+
+void qsort2(std::vector<int>& vec, int low, int high);
+void print(std::vector<int>& vec);
+int median(std::vector<int>& vec, int x, int y, int z);
+void swap(std::vector<int>& v, int low, int index);
